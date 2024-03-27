ee70df3628d7245db9a23a9fb14264a663f7ed9e
sumitsharma <sumitkumar.codeinit@gmail.com>
2024-03-27 18:50:52 +0530

    intial commit

--- first.php
+++ first.php
@@ -7,8 +7,8 @@
         $productShow = Product::all();
         $categories = Category::all();
         
-        //return view('front.home', compact('categories'));
-        
+        return view('front.home', compact('categories'));
+        return view('front.home', compact('categories'));
 
         return view('front.home',['categories' => $categories]);
     }
